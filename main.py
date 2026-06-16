43dc819d333712ff891e5777201408a5f6ffe656
Mauro B <mauroalessandrobuehler@gmail.com>
2026-06-17 01:05:52 +0200

    added the fps limit

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    
+    clock = pygame.time.Clock() #Clock and delta time for FPS
+    dt: float = 0.0
+    
+    while True:         #The game loop, this updates the screen and check for user input... I think
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
         
-        for event in pygame.event.get():
+        for event in pygame.event.get():    #makes it so the game doesn't need ctrl + c, but the x button to close
             if event.type == pygame.QUIT:
                 return
+        
+        
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
