25e9d5a46166454267f92821570d3317807216bf
Mauro B <mauroalessandrobuehler@gmail.com>
2026-06-17 02:34:35 +0200

    added movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from pygame import Surface
 from circleshape import *
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float,):
@@ -18,13 +18,23 @@ class Player(CircleShape):
     def draw(self, screen: pygame.Surface) -> None:     #draws player sprite
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
-    def rotate(self, dt: float) -> None:        # rotater
+    def rotate(self, dt: float) -> None:        # rotater method
         self.rotation += PLAYER_TURN_SPEED * dt
         
+    def move(self, dt: float) -> None:      # mover method
+        unit_vector = pygame.Vector2(0,1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        final_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += final_vector
+
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
-            self.rotate(dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_a]:    # rotation
             self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:    # movement
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
